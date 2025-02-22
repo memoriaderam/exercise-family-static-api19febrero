41010651cd63c0b70d025061ff26987df0b8e382
Roberto Agredo <memoriaderam@gmail.com>
2025-02-22 16:35:18 +0000

    Update finished

--- src/app.py
+++ src/app.py
@@ -15,6 +15,33 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+John = {
+    "first_name": "John",
+    "last_name": jackson_family.last_name,
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+}
+
+Jane = {
+     "first_name": "Jane",
+     "last_name": jackson_family.last_name,
+     "age": 35, 
+     "lucky_numbers": [10,14,3]
+}
+
+Jimmy = {
+      "first_name": "Jimmy",
+      "last_name": jackson_family.last_name,
+      "age": 5,
+      "lucky_numbers":[1]
+}
+
+jackson_family.add_member(John)
+jackson_family.add_member(Jimmy)
+jackson_family.add_member(Jane)
+
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -26,19 +53,36 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
+def get_all_members():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
+
+@app.route('/member/<int:id>',methods =['GET'])
+def get_single_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+
+@app.route('/member', methods=['POST'])
+def add_new():
+    member = request.json
+    jackson_family.add_member(member)
+    if member is not None:
+        return "member created",200
 
 
-    return jsonify(response_body), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])    
+def delete_member(id):
+    member = jackson_family.get_member(id)
+    if member:
+        jackson_family.delete_member(id)
+        return jsonify({"message": f"Member deleted successfully: {member}"}), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404 
 
 # this only runs if `$ python src/app.py` is executed
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -8,27 +8,53 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
-        # example list of members
+        self._next_id = 1
         self._members = []
+        # example list of members
+       
 
     # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
-        return randint(0, 99999999)
+   
+    
+    def _generate_id(self):
+        generated_id = self._next_id
+        self._next_id += 1
+        return generated_id
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member["last_name"] = self.last_name
+        member["id"] = self._generate_id()
+        member["lucky_numbers"] = list(member.get("lucky_numbers", set()))
+        self._members.append(member)
+        return member
+        ## Debes implementar este método
+        ## Agrega un nuevo miembro a la lista de _members
+    
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+         for position in range(len(self._members)):
+            if self._members[position]["id"] == id:
+                self._members.pop(position)
+                
+                return None
+
+        ## Debes implementar este método
+        ## Recorre la lista y elimina el miembro con el id proporcionado
+       
 
     def get_member(self, id):
-        # fill this method and update the return
+        for member in self._members:
+            if member["id"] == int(id):
+                return member
+            
+        return None
+        ## Debes implementar este método
+        ## Recorre la lista y obtén el miembro con el id proporcionado
         pass
 
     # this method is done, it returns a list with all the family members
